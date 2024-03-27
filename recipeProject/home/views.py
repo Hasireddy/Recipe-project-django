aa2b9a361bed2a7a0e7a0ba15cff7a089a05bb45
Hasireddy <malikireddy.uma@gmail.com>
2024-03-27 10:47:42 +0100

    html template rendered

--- recipeProject/home/views.py
+++ recipeProject/home/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# def home(request):
+#     return HttpResponse("""<h1 style="color:red">This is a recipe project</h1>
+#                         <hr>
+#                         <p>This is coming from django server</p>""")
+
+
 def home(request):
-    return HttpResponse("This is a recipe project")
+    return render(request,'index.html')
